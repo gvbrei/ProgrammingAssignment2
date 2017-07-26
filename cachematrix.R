e01e31115bbd3f0e026fcc96bc0a2bde582a1e0f
gvbrei <30450733+gvbrei@users.noreply.github.com>
2017-07-26 15:26:34 -0400

    Revert "Update cachematrix.R"

--- cachematrix.R
+++ cachematrix.R
@@ -1,36 +1,15 @@
-#Using the example provided for vector means, I realized that the sample principles apply to matrix inverses. 
+## Put comments here that give an overall description of what your
+## functions do
 
-## creates a special "matrix", which is really a list containing a function to set the value of the matrix, get the value of the matrix,
-##set the value of the inverse and get the value of the inverse
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <-function(y) {
-    x <<- y ## <<- assigns values to objects in a different environment
-    m <<- NULL
-  }
-  get <- function() x
-  setinverse <-function(inverse) m <<- inverse
-  getinverse <- function() m 
-  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
-  
+
 }
 
 
-## The following takes the output of makeCacheMatrix and returns an inverse of the orginal matrix input
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  m <-x$getinverse()
-  
-  if(!is.null(m)){ ##if the inverse has already been calculated
-    message("Retrieving cached data")
-    return(m)
-  }
-  data <- x$get() ##if the inverse has not already been calculated, this calculates the inverse
-  m <- solve(data, ...)
-  
-  x$setinverse(m) #Sets the value of the inverse in the cache
-  m #return m 
 }
-
